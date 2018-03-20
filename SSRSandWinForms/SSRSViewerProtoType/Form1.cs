1b73c90117c1215deff698ed2d30fca1a0c6af9c
Rob Blake <rob_bl8ke@hotmail.com>
2018-03-20 11:33:47 +0200

    Moved some functionality out of the Report component.

--- SSRSandWinForms/SSRSViewerProtoType/Form1.cs
+++ SSRSandWinForms/SSRSViewerProtoType/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using SSRSViewerPrototType.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,7 +22,14 @@ namespace SSRSViewerProtoType
 
         private void btnLoad_Click(object sender, EventArgs e)
         {
-            reportViewer1.LoadReport();
+            WeightVsLimitScheduleRepository repository = new WeightVsLimitScheduleRepository("server=ROB-LT;database=SPC_DEV_H4CI;Integrated Security=True;;Connection Reset=true;");
+            repository.Generate();
+
+            //reportViewer1.LoadReport();\
+            reportViewer1.ReportPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports\WeightVsLimitSchedule.rdlc");
+            reportViewer1.AddDataSet("WeightVsLimitScheduleDataSet", repository.Get());
+
+            reportViewer1.Render();
         }
     }
 }
--- SSRSandWinForms/SsrsViewer/ReportViewer.cs
+++ SSRSandWinForms/SsrsViewer/ReportViewer.cs
@@ -1,60 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
+﻿using Microsoft.Reporting.WinForms;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.Reporting.WinForms;
-using System.Reflection;
-using System.IO;
-using System.Data.SqlClient;
-using SsrsViewer.Repositories;
 
 namespace SsrsViewer
 {
     public partial class ReportViewer: UserControl
     {
+        private LocalReport localReport;
+
         public ReportViewer()
         {
             InitializeComponent();
+            reportViewer1.ProcessingMode = ProcessingMode.Local;
+            this.localReport = reportViewer1.LocalReport;
         }
 
       
-
-        public void LoadReport()
+        public string ReportPath
         {
-
-            // ***************************************************************************
-            //BreachHistoryReportRepository repository = new BreachHistoryReportRepository("server=ROB-LT;database=SPC_DEV_H4CI;Integrated Security=True;;Connection Reset=true;");
-            //repository.Generate();
-
-            WeightVsLimitScheduleRepository repository = new WeightVsLimitScheduleRepository("server=ROB-LT;database=SPC_DEV_H4CI;Integrated Security=True;;Connection Reset=true;");
-            repository.Generate();
-
-            // ***************************************************************************
-            // Using the Report Viewer...
-            // ***************************************************************************
-            reportViewer1.ProcessingMode = ProcessingMode.Local;
-            LocalReport viewerReport = reportViewer1.LocalReport;
-            viewerReport.ReportPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports\WeightVsLimitSchedule.rdlc");
-            viewerReport.DataSources.Add(new ReportDataSource("WeightVsLimitScheduleDataSet", repository.Get()));
-
-            reportViewer1.RefreshReport();
-            // ***************************************************************************
-
-
-            // ***************************************************************************
-            // Using the Exporting Results
-            // ***************************************************************************
-            LocalReport exportReport = new LocalReport();
-            exportReport.ReportPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports\WeightVsLimitSchedule.rdlc");
-            exportReport.DataSources.Add(new ReportDataSource("WeightVsLimitScheduleDataSet", repository.Get()));
-            byte[] data = exportReport.Render("PDF");
-            File.WriteAllBytes(@"C:\WeightVsLimitScheduleDataSet.pdf", data);
-            // ***************************************************************************
+            get { return localReport.ReportPath; }
+            set { this.localReport.ReportPath = value; }
         }
+        
+        public void AddDataSet(string dataSetName, DataTable dataTable)
+        {
+            localReport.DataSources.Add(new ReportDataSource(dataSetName, dataTable));
+        }
+
+        public void Render()
+        {
+            reportViewer1.RefreshReport();
+        }
+
+
+        //public void LoadReport()
+        //{
+
+        //    // ***************************************************************************
+        //    //BreachHistoryReportRepository repository = new BreachHistoryReportRepository("server=ROB-LT;database=SPC_DEV_H4CI;Integrated Security=True;;Connection Reset=true;");
+        //    //repository.Generate();
+
+        //    WeightVsLimitScheduleRepository repository = new WeightVsLimitScheduleRepository("server=ROB-LT;database=SPC_DEV_H4CI;Integrated Security=True;;Connection Reset=true;");
+        //    repository.Generate();
+
+        //    // ***************************************************************************
+        //    // Using the Report Viewer...
+        //    // ***************************************************************************
+        //    reportViewer1.ProcessingMode = ProcessingMode.Local;
+        //    LocalReport viewerReport = reportViewer1.LocalReport;
+        //    viewerReport.ReportPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports\WeightVsLimitSchedule.rdlc");
+        //    viewerReport.DataSources.Add(new ReportDataSource("WeightVsLimitScheduleDataSet", repository.Get()));
+
+        //    reportViewer1.RefreshReport();
+        //    // ***************************************************************************
+
+
+        //    // ***************************************************************************
+        //    // Using the Exporting Results
+        //    // ***************************************************************************
+        //    LocalReport exportReport = new LocalReport();
+        //    exportReport.ReportPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports\WeightVsLimitSchedule.rdlc");
+        //    exportReport.DataSources.Add(new ReportDataSource("WeightVsLimitScheduleDataSet", repository.Get()));
+        //    byte[] data = exportReport.Render("PDF");
+        //    File.WriteAllBytes(@"C:\WeightVsLimitScheduleDataSet.pdf", data);
+        //    // ***************************************************************************
+        //}
     }
 }
