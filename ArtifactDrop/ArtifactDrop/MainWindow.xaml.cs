50da37958de56ba2a77ca051a2aa902c2754c92f
Rob Blake <rob_bl8ke@hotmail.com>
2018-03-23 11:20:03 +0200

    Added ArtifactDrop.csproj

---
+++ ArtifactDrop/ArtifactDrop/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArtifactDrop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+            this.Activated += MainWindow_Activated;
+            this.MouseDown += MainWindow_MouseDown;
+            //this.Drop += MainWindow_Drop;
+
+        }
+
+        // Draw your own target: https://jeremybytes.blogspot.co.za/2009/03/wpf-xaml-sample.html
+        // TaskBar Stuff (This Library - see Nuget - and comprehensive tutorial at https://www.codeproject.com/Articles/36468/WPF-NotifyIcon)
+
+        private void Image_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+                HandleFileOpen(files[0]);
+            }
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void MainWindow_Activated(object sender, EventArgs e)
+        {
+            this.Topmost = true;
+            this.Activate();
+        }
+
+        private void HandleFileOpen(string file)
+        {
+
+        }
+
+        private void Image_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+    }
+}
