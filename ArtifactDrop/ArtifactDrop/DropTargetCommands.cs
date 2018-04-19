336f9ad4482b4d2142a5d155278a2b8b10ff7143
Rob Blake <rob_bl8ke@hotmail.com>
2018-04-19 11:54:44 +0200

    Added shortcuts to close menu item.

--- ArtifactDrop/ArtifactDrop/DropTargetCommands.cs
+++ ArtifactDrop/ArtifactDrop/DropTargetCommands.cs
@@ -10,12 +10,12 @@ namespace ArtifactDrop
     public static class DropTargetCommands
     {
         public static readonly RoutedUICommand Exit = new RoutedUICommand("Close", "Close", typeof(DropTargetCommands)
-        //    , 
-        //    new InputGestureCollection()
-        //{
-        //    new KeyGesture(Key.F4, ModifierKeys.Alt),
-        //    new KeyGesture(Key.Q, ModifierKeys.Control, "Crl + Q")
-        //}
+            ,
+            new InputGestureCollection()
+        {
+            new KeyGesture(Key.F4, ModifierKeys.Alt),
+            new KeyGesture(Key.Q, ModifierKeys.Control, "Crl + Q")
+        }
         );
     }
 }
