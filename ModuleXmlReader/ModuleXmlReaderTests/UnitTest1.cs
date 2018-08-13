29e0e0fd0edab8479e9b6079ea4680336afa2f8d
Rob Blake <robbie.a.blake@gmail.com>
2018-08-13 10:14:16 +0200

    Quick commit of added commented section.

--- ModuleXmlReader/ModuleXmlReaderTests/UnitTest1.cs
+++ ModuleXmlReader/ModuleXmlReaderTests/UnitTest1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Xml.Linq;
 using System.Collections.Generic;
 using ModuleXmlReader;
+using System.Text;
 
 namespace ModuleXmlReaderTests
 {
@@ -63,6 +64,38 @@ namespace ModuleXmlReaderTests
 
             Assert.IsTrue(menuSystem.RootMenu != null);
         }
+
+        //[TestMethod]
+        //public void RipModules()
+        //{
+        //    string section = "Section";
+        //    string subsection = "SubSection";
+        //    ModuleMenuReader menuSystem = new ModuleMenuReader("TestModule.xml");
+
+        //    StringBuilder builder = new StringBuilder();
+        //    string folderPath = @"C:\Personal\InfoVest\SPC\History\2018\07\SPC Command Menu System\SearchModule";
+        //    IEnumerable<string> files = System.IO.Directory.EnumerateFiles(folderPath, "*.xml");
+
+        //    foreach (var file in files)
+        //    {
+        //        foreach (var item in menuSystem.RootMenu.Menus)
+        //        {
+        //            if (item.Menus.Length > 0)
+        //            {
+        //                foreach (var subItem in item.Menus)
+        //                {
+        //                    builder.AppendLine($"{section},{subsection},{subItem.Command},{item.Text} -> {subItem.Text}");
+        //                    //System.Diagnostics.Debug.WriteLine($"{section},{subsection},{subItem.Command},{item.Text} -> {subItem.Text}");
+        //                }
+        //            }
+        //            else
+        //                builder.AppendLine($"{section},{subsection},{item.Command},{item.Text}");
+        //                //System.Diagnostics.Debug.WriteLine($"{section},{subsection},{item.Command},{item.Text}");
+        //        }
+        //    }
+
+        //    Assert.IsTrue(menuSystem.RootMenu != null);
+        //}
 
         [TestMethod]
         public void ReadsXmlFileSuccessfully()
