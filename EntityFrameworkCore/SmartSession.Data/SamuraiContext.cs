ce283256c2e246777651c00cacd8af46d455523c
Rob Blake <robbie.a.blake@gmail.com>
2018-08-15 14:06:37 +0200

    Creating a Data Model and Database with EF Core.

---
+++ EntityFrameworkCore/SmartSession.Data/SamuraiContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSession.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSession.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        public SamuraiContext()
+        {
+                
+        }
+        // for dependency injection...
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // or sql lite or sql ce
+            optionsBuilder.UseSqlServer(@"server=ROBB-LT02\ROBLT;database=SmartSession_EF;Integrated Security=True;Connection Reset=true;");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // maps a many to many relationship...
+            modelBuilder.Entity<SamuraiBattle>()
+                .HasKey(s => new { s.SamuraiId, s.BattleIdId });
+        }
+    }
+}
---
+++ EntityFrameworkCore/SmartSession.Domain/Samurai.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSession.Domain
+{
+    public class Samurai
+    {
+        public Samurai()
+        {
+            Quotes = new List<Quote>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; }
+        List<SamuraiBattle> SamuraiBattles { get; set; }
+        public SecretIdentity SecretIdentity { get; set; }
+    }
+
+    public class Quote
+    {
+        public int Id { get; set; }
+        public Samurai Samurai { get; set; }
+        public int SamuraiId { get; set; }
+    }
+
+    public class Battle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<SamuraiBattle> SamuraiBattles { get; set; }
+    }
+
+    public class SamuraiBattle
+    {
+        public int BattleIdId { get; set; }
+        public int SamuraiId { get; set; }
+        public Samurai Samurai { get; set; }
+        public Battle Battle { get; set; }
+    }
+
+    public class SecretIdentity
+    {
+        public int Id { get; set; }
+        public string RealName { get; set; }
+        public int SamuraiId { get; set; }
+    }
+}
