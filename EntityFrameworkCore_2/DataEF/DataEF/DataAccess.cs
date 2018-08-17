4adf06336c367cf7328ebcb3048f8cd094fb4ec6
Rob Blake <robbie.a.blake@gmail.com>
2018-08-17 14:09:00 +0200

    A working entity framework version!

---
+++ EntityFrameworkCore_2/DataEF/DataEF/DataAccess.cs
@@ -0,0 +1,46 @@
+﻿using SmartSession.Domain.Records;
+using System;
+
+namespace CygSoft.SmartSession.EF
+{
+    public class DataAccess
+    {
+        public void InsertGoal(Goal goal)
+        {
+            if (goal.Id > 0)
+                throw new ArgumentException("A new goal cannot have an id");
+
+            using (var context = new SmartSessionContext())
+            {
+                context.Goals.Add(goal);
+                // context.Add(goal); // for polymorphic inserts.
+                // context is now tracking the goal.
+                context.SaveChanges(); // now is saved.
+            }
+        }
+
+        public void InsertExercise(Exercise exercise)
+        {
+            if (exercise.Id > 0)
+                throw new ArgumentException("A new exercise cannot have an id");
+
+            using (var context = new SmartSessionContext())
+            {
+                context.Exercises.Add(exercise);
+                context.SaveChanges();
+            }
+        }
+
+        public void InsertTask(PracticeTask task)
+        {
+            if (task.Id > 0)
+                throw new ArgumentException("A new task cannot have an id");
+
+            using (var context = new SmartSessionContext())
+            {
+                context.Tasks.Add(task);
+                context.SaveChanges();
+            }
+        }
+    }
+}
