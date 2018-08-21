095cbf2760bb698ceeee1eec29281436ab7f4d19
Rob Blake <robbie.a.blake@gmail.com>
2018-08-21 09:12:19 +0200

    First basic code base to work with MVVM Light including a goal listview.

---
+++ MVVM Light/MvvmLight_Prototypes/MvvmLight_Prototypes/Model/GoalListItem.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmLight_Prototypes.Model
+{
+    public class GoalListItem : ObservableObject
+    {
+        public string Title { get; set; }
+        public int Progress { get; set; }
+
+        public int Days { get; set; }
+
+        public bool OnSchedule { get; set; }
+
+        public override void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            base.RaisePropertyChanged(propertyExpression);
+        }
+
+        public override void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.RaisePropertyChanged(propertyName);
+        }
+    }
+}
