0e73ed62bebe1afd2c9b81592cc12843dfce39b6
Rob Blake <robbie.a.blake@gmail.com>
2018-08-23 11:50:21 +0200

    Added STARS to the exercise search list.

--- MVVM Light/MvvmLight_Prototypes/MvvmLight_Prototypes/ViewModel/ExerciseSearchViewModel.cs
+++ MVVM Light/MvvmLight_Prototypes/MvvmLight_Prototypes/ViewModel/ExerciseSearchViewModel.cs
@@ -11,9 +11,9 @@ namespace MvvmLight_Prototypes.ViewModel
             new ExerciseListItem { Title = "Guitar Solo School: Beginner Lead Guitar Method - Chapter 2: Building Melodies - 2d",
                 DifficultyRating = 3, FavouriteRating = 4, Scribed = false, SuggestedDuration = 120 },
             new ExerciseListItem { Title = "Guitar Solo School: Beginner Lead Guitar Method - Chapter 3: Slides - 3d",
-                DifficultyRating = 3, FavouriteRating = 4, Scribed = false, SuggestedDuration = 120  },
+                DifficultyRating = 3, FavouriteRating = 2, Scribed = false, SuggestedDuration = 120  },
             new ExerciseListItem { Title = "Guitar Solo School: Beginner Lead Guitar Method - Chapter 4: Core Bending Licks - Bends around the E String - 4t",
-                DifficultyRating = 3, FavouriteRating = 4, Scribed = false, SuggestedDuration = 120  },
+                DifficultyRating = 3, FavouriteRating = 1, Scribed = false, SuggestedDuration = 120  },
             new ExerciseListItem { Title = "Guitar Solo School: Beginner Lead Guitar Method - Chapter 5: Legato - One Fret Pull Off - 5i",
                 DifficultyRating = 3, FavouriteRating = 4, Scribed = false, SuggestedDuration = 120  }
         };
