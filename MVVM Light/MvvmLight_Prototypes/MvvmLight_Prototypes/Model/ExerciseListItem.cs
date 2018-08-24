ccb689c9c197cd470bc35f1a8f9c4a922be2516e
Rob Blake <robbie.a.blake@gmail.com>
2018-08-24 14:16:35 +0200

    Bind ListView to the SelectedExercise.

--- MVVM Light/MvvmLight_Prototypes/MvvmLight_Prototypes/Model/ExerciseListItem.cs
+++ MVVM Light/MvvmLight_Prototypes/MvvmLight_Prototypes/Model/ExerciseListItem.cs
@@ -4,46 +4,77 @@ namespace MvvmLight_Prototypes.Model
 {
     public class ExerciseListItem : ObservableObject
     {
+        private bool isDirty;
+        public bool IsDirty
+        {
+            get { return isDirty; }
+            set { Set(() => IsDirty, ref isDirty, value); }
+        }
+
         private string title;
         public string Title
         {
             get { return title; }
-            set { Set(() => Title, ref title, value); }            
+            set
+            {
+                if (Set(() => Title, ref title, value))
+                    isDirty = true;
+            }            
         }
 
         private int suggestedDuration;
         public int SuggestedDuration
         {
             get { return suggestedDuration; }
-            set { Set(() => SuggestedDuration, ref suggestedDuration, value); }
+            set
+            {
+                if (Set(() => SuggestedDuration, ref suggestedDuration, value))
+                    isDirty = true;
+            }
         }
 
         private int difficultyRating;
         public int DifficultyRating
         {
             get { return difficultyRating; }
-            set { Set(() => DifficultyRating, ref difficultyRating, value); }
+            set
+            {
+                if (Set(() => DifficultyRating, ref difficultyRating, value))
+                    isDirty = true;
+            }
         }
 
-        private int favouriteRating;
-        public int FavouriteRating
+        private int practicalityRating;
+        public int PracticalityRating
         {
-            get { return favouriteRating; }
-            set { Set(() => FavouriteRating, ref favouriteRating, value); }
+            get { return practicalityRating; }
+            set
+            {
+                if (Set(() => PracticalityRating, ref practicalityRating, value))
+                    isDirty = true;
+            }
         }
 
         private bool scribed;
         public bool Scribed
         {
             get { return scribed; }
-            set { Set(() => Scribed, ref scribed, value); }
+            set
+            {
+                if (Set(() => Scribed, ref scribed, value))
+                    isDirty = true;
+            }
         }
 
         private string notes;
         public string Notes
         {
             get { return notes; }
-            set { Set(() => Notes, ref notes, value); }
+            set
+            {
+                if (Set(() => Notes, ref notes, value))
+                    isDirty = true;
+            }
         }
     }
 }
--- MVVM Light/MvvmLight_Prototypes/MvvmLight_Prototypes/ViewModel/ExerciseSearchViewModel.cs
+++ MVVM Light/MvvmLight_Prototypes/MvvmLight_Prototypes/ViewModel/ExerciseSearchViewModel.cs
@@ -1,21 +1,47 @@
 ﻿using GalaSoft.MvvmLight;
 using MvvmLight_Prototypes.Model;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace MvvmLight_Prototypes.ViewModel
 {
     public class ExerciseSearchViewModel : ViewModelBase
     {
+
+        //public ExerciseSearchViewModel(IExerciseService exerciseService, IDialogService dialogService, INavigationService navigationService)
+        //{
+        //    this.exerciseService = exerciseService;
+        //    this.dialogService = dialogService;
+        //    this.navigationService = navigationService;
+        //}
+
+        // for blend:
+        //public ExerciseSearchViewModel() : this(new ExerciseService(), new DialogService(), new NavigationService())
+        //{
+        //    ...
+        //}
+
+
+        private ExerciseListItem selectedExercise;
+        public ExerciseListItem SelectedExercise
+        {
+            get { return selectedExercise; }
+            set
+            {
+                Set(() => SelectedExercise, ref selectedExercise, value);
+            }
+        }
+
         public ObservableCollection<ExerciseListItem> ExerciseList { get; } = new ObservableCollection<ExerciseListItem>()
         {
             new ExerciseListItem { Title = "Guitar Solo School: Beginner Lead Guitar Method - Chapter 2: Building Melodies - 2d",
-                DifficultyRating = 3, FavouriteRating = 4, Scribed = false, SuggestedDuration = 120 },
+                DifficultyRating = 3, PracticalityRating = 4, Scribed = false, SuggestedDuration = 120, Notes="Here is a sample note" },
             new ExerciseListItem { Title = "Guitar Solo School: Beginner Lead Guitar Method - Chapter 3: Slides - 3d",
-                DifficultyRating = 3, FavouriteRating = 2, Scribed = false, SuggestedDuration = 120  },
+                DifficultyRating = 3, PracticalityRating = 2, Scribed = false, SuggestedDuration = 120, Notes="Get those fingers working, start from 60 to 80 BPM."  },
             new ExerciseListItem { Title = "Guitar Solo School: Beginner Lead Guitar Method - Chapter 4: Core Bending Licks - Bends around the E String - 4t",
-                DifficultyRating = 3, FavouriteRating = 1, Scribed = false, SuggestedDuration = 120  },
+                DifficultyRating = 3, PracticalityRating = 1, Scribed = false, SuggestedDuration = 120, Notes = "Start at 80 BPM, you really want to get to 120 BPM."  },
             new ExerciseListItem { Title = "Guitar Solo School: Beginner Lead Guitar Method - Chapter 5: Legato - One Fret Pull Off - 5i",
-                DifficultyRating = 3, FavouriteRating = 4, Scribed = false, SuggestedDuration = 120  }
+                DifficultyRating = 3, PracticalityRating = 4, Scribed = false, SuggestedDuration = 120  }
         };
     }
 }
